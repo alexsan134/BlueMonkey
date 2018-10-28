797719e2f10c39c90b26e49f1ca3b8590a6df47d
Alex Santos <as62971@gmail.com>
2018-10-28 19:56:31 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 //SideNav
 g("#menuBtn").event("click",() =>{
 	g(".sideNav").css("display:block");
-	g("main").animates("filter:blur(3px);transform:scale(0.95, 0.95)");
+	g("main").animates("transform:scale(0.95, 0.95)");
 	setTimeout(() =>{
 		g("#shadow").animates("opacity:1", 700);
 		g("#contain").animates("right:0");		
@@ -11,7 +11,7 @@ g("#menuBtn").event("click",() =>{
 g("#shadow").event("click",() =>{
 	g("#shadow").animates("opacity:0", 700);
 	g("#contain").css("right:-300px");
-	g("main").animates("filter:none;transform:none");
+	g("main").animates("transform:none");
 	setTimeout(() =>{
 		g(".sideNav").css("display:none");		
 	}, 700)
